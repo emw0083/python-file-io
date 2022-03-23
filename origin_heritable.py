349946a2669aa8c530eab82a973d16bcb5e79ac5
Emily M. Wilkins <emw0083@auburn.edu>
2022-03-23 09:48:28 -0500

    editing file

--- origin_heritable.py
+++ origin_heritable.py
@@ -16,8 +16,8 @@ read
 
 # create empty list
 arr = []
- 
 line = 1
+
 for word in read:
     if word == '\n':
         line += 1
@@ -26,13 +26,23 @@ print("Number of lines in file is: ", line)
 for i in range(line):
     arr.append(file.readline())
 
-#def findline(word):
-#    for i in range(len(arr)):
-#        if word in arr[i]:
-#            print(i+1, end=", ")
-            
+hertiable =
+
 findline('herit')
 
-
-
 file.close()
+
+
+print('Opening dummy.txt')
+with open('dummy.txt', 'r') as in_stream:
+    print('Opening output.txt')
+    with open('output.txt', 'w') as out_stream:
+        for line in in_stream:
+            line = line.strip()
+            word_list = line.split()
+            word_list.sort()
+            for word in word_list:
+                out_stream.write('{0}\n'.format(word))
+print("Done!")
+print('dummy.txt is closed?', in_stream.closed)
+print('output.txt is closed?', out_stream.closed)
